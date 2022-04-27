18ab5a6518442219c2f365b0ff58c33b34d5789d
hermelamichael <99145797+hermelamichael@users.noreply.github.com>
2022-04-27 14:22:11 -0500

    first commit

---
+++ testing.py
@@ -0,0 +1,14 @@
+'''
+myString = 'Carlos "Cabeto" Rincon' #cant have double qoutes isnide already double qouted unless you use escape charcters // before "
+print(myString[0]) #introduction to index #this is important because now we can use this instead of remainder 10
+'''
+
+'''
+myString = "1234"
+if myString[3] == "4":
+    print("They are equal")
+
+#concatenation > 
+'''
+
+print(13/2)
